d3461cdc6d0b6ea25cf9ae069cc530b287f6c12c
DmytroCherednichenko <creator1517@gmail.com>
2025-07-23 13:31:10 -0300

    update homepage

--- src/pages/homepage/Homepage.jsx
+++ src/pages/homepage/Homepage.jsx
@@ -1,26 +1,31 @@
-import { Container, Spinner, Carousel } from "react-bootstrap"
+import { Container, Carousel } from "react-bootstrap"
 import "./homepage.css"
-import { useState } from "react";
-import CarouselImage from "../../components/carouselimage/CarouselImage";
 import TopSellers from "../../components/topsellers/TopSellers";
+import carousel_image_1 from "../../assets/hero-image.jpg"
+import carousel_image_2 from "../../assets/car_img_2.jpg"
+import carousel_image_3 from "../../assets/car_img_1.jpg"
 
 const Homepage = () => {
-    const [loading, setLoading] = useState(false);
 
-    if (loading) return <Container className="homepage-loading"><Spinner animation="border" variant="secondary" /></Container>
 
     return (
         <Container className="homepage-page">
             <Container fluid className="carousel-wrapper">
                 <Carousel>
                     <Carousel.Item>
-                        <CarouselImage image="src/assets/hero-image.jpg" />
+                        <div className="carousel-image-wrapper">
+                            <img className="carousel-image" src={carousel_image_1} alt="" />
+                        </div>
                     </Carousel.Item>
                     <Carousel.Item>
-                        <CarouselImage image="src/assets/car_img_2.jpg" />
+                        <div className="carousel-image-wrapper">
+                            <img className="carousel-image" src={carousel_image_2} alt="" />
+                        </div>
                     </Carousel.Item>
                     <Carousel.Item>
-                        <CarouselImage image="src/assets/car_img_1.jpg" />
+                        <div className="carousel-image-wrapper">
+                            <img className="carousel-image" src={carousel_image_3} alt="" />
+                        </div>
                     </Carousel.Item>
                 </Carousel>
             </Container>
